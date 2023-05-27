536f95c26cba585d1d48543ad23074dfb13221e4
Viktor <viktorhino@gmail.com>
2023-05-27 12:29:10 -0500

    Adición de la funcion desencriptar

--- js/criptext.js
+++ js/criptext.js
@@ -24,3 +24,29 @@ function encriptar(){
     }
 
 }
+
+function desencriptar() {
+    let textoOrigen = document.getElementById("txt-origen").value;
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let parrafoMensaje = document.getElementById("parrafo-mensaje");
+    let imagenMensaje = document.getElementById("img-mensaje");
+    
+    let textoDesencriptado = textoOrigen
+                            .replace(/arx/gi,"a")
+                            .replace(/enter/gi,"e")
+                            .replace(/imes/gi,"i")
+                            .replace(/ober/gi,"o")
+                            .replace(/ufat/gi,"u");
+
+    if(textoOrigen.length != 0){
+        document.getElementById("txt-origen").value = textoDesencriptado;
+        tituloMensaje.textContent = "Texto desencriptado con éxito";
+        parrafoMensaje.textContent = ""
+        imagenMensaje.src = "img/desencriptado-ok.png"
+    } else {
+        imagenMensaje.src = "img/inicial.png"
+        tituloMensaje.textContent = "Ningún mensaje encontrado"
+        parrafoMensaje.textContent = "Ingrese el texto que desea encriptar o desencriptar"
+        alert("Debe ingresar algún texto");
+    }                            
+}
